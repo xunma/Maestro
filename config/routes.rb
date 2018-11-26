4048c1810f808e7ae4969b0b79abcc2fe4724725
Annie Gilchrist <anneliese.gilchrist@gmail.com>
2018-11-26 17:31:51 +0100

    created models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :rooms do
+    collection do
+      get 'search'
+    end
+
+    member do
+      get 'bookings', to: 'rooms#bookings'
+    end
+
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
 end
