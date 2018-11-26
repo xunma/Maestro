a91dfc84b94aa2d5cf76b88e12a36781944267ed
Ian Hunter <hunteia@gmail.com>
2018-11-26 18:20:46 +0100

    adding validations to models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,10 @@
 class Room < ApplicationRecord
+  validates :instrument_type, presence: true
+  validates :description, presence: true, allow_blank: false
+  validates :location, presence: true, allow_blank: false
+  validates :price, presence: true, allow_blank: false
+  validates :user_id, presence: true, allow_blank: false
+  has_many :room_images, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   belongs_to :user
 end
--- app/models/room_image.rb
+++ app/models/room_image.rb
@@ -1,3 +1,5 @@
 class RoomImage < ApplicationRecord
+  validates :room_images, presence: true
+  validates :room_id, presence: true, allow_blank: false
   belongs_to :room
 end
