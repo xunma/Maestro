dce4e8c683fa958089016c5e9dadb1dcd0f24bc3
Xun Ma <lazybone1995@hotmail.com>
2018-11-27 12:56:44 +0100

    add showing related bookings method in room controller, added create room form without photo upload function in views.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_093402) do
+ActiveRecord::Schema.define(version: 2018_11_27_101246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_093402) do
     t.bigint "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["room_id"], name: "index_room_images_on_room_id"
   end
 
