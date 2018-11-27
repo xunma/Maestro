44188742bf4094274934b68d912252e2c00cffce
Xun Ma <lazybone1995@hotmail.com>
2018-11-27 18:34:56 +0100

    clean out schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_131202) do
+ActiveRecord::Schema.define(version: 2018_11_27_103325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_131202) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "images"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
